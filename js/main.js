0c099d6c6dbdd5affefcc155d397dd2901c217c2
twishasaraiya <saraiyatwisha@gmail.com>
2018-06-28 10:20:56 +0530

    Fixed certain typos

--- js/main.js
+++ js/main.js
@@ -149,7 +149,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = restaurant.name;
   li.append(image);
 
-  const name = document.createElement('h2'); //restaurant-name has less semantic importance than page title
+  const name = document.createElement('h3'); //restaurant-name has less semantic importance than page title
   name.innerHTML = restaurant.name;
   li.append(name);
 
