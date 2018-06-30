4f0ec638457b63a31247d8c0a6dbc087ea688e73
twishasaraiya <saraiyatwisha@gmail.com>
2018-06-30 11:05:23 +0530

    Created register-sw.js

---
+++ register-sw.js
@@ -0,0 +1,9 @@
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('sw.js')
+    .then(function(registeration){
+      console.log('Service Worker registered');
+    })
+    .catch(function(error){
+      console.log('Failed to register Service Worker');
+    });
+}
