59a85127a14329e2920a9ebb78b6259160ce0374
twishasaraiya <saraiyatwisha@gmail.com>
2018-09-13 19:28:14 +0530

    Live Reload changes using browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -12,111 +12,112 @@ var cleanCSS = require('gulp-clean-css')
 var rename = require('gulp-rename')
 var log = require('gulplog')
 var bs = require('browser-sync').create()
+var reload = bs.reload
 // vinyl => virtual file format for gulp streams
 
 var mainPageFiles = ['src/js/dbhelper.js', 'src/js/main.js']
 var restaurantInfoPageFiles = [
-	'src/js/dbhelper.js',
-	'src/js/restaurant_info.js'
+  'src/js/dbhelper.js',
+  'src/js/restaurant_info.js'
 ]
 
 /*
   * Convert Images to Next-Gen format Webp
   */
 gulp.task('images', () => {
-	gulp
-		.src('public/img/*.jpg')
-		.pipe(webp())
-		.pipe(gulp.dest('dist/img/'))
+  gulp
+    .src('public/img/*.jpg')
+    .pipe(webp())
+    .pipe(gulp.dest('dist/img/'))
 })
 
 /*
 * Copy files to dist folder
 */
 gulp.task('copy', () => {
-	gulp.src('index.html').pipe(gulp.dest('dist/'))
+  gulp.src('index.html').pipe(gulp.dest('dist/'))
 })
 /*
    * Convert es6 to es2015
    * https://github.com/gulpjs/gulp/blob/master/docs/recipes/browserify-uglify-sourcemap.md
    * https://travismaynard.com/writing/using-babel-with-gulp
    */
-gulp.task('build', () => {
-	mainPageFiles.map(jsfile => {
-		return (
-			browserify({
-				entries: [jsfile]
-			})
-				.transform('babelify', { presets: ['env'] })
-				.bundle()
-				.pipe(source('main.bundle.js')) // source works only for one file at a time
-				.pipe(buffer()) // convert streaming to buffered vinyl file objects
-				.pipe(sourcemaps.init({ loadMaps: true }))
-			// Add transformation tasks to the pipeline here.
-				.pipe(uglify())
-				.on('error', log.error)
-				.pipe(sourcemaps.write('./'))
-				.pipe(gulp.dest('dist/js/'))
-		)
-	})
+gulp.task('minify-js', () => {
+  mainPageFiles.map(jsfile => {
+    return (
+      browserify({
+        entries: [jsfile]
+      })
+        .transform('babelify', { presets: ['env'] })
+        .bundle()
+        .pipe(source('main.bundle.js')) // source works only for one file at a time
+        .pipe(buffer()) // convert streaming to buffered vinyl file objects
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        // Add transformation tasks to the pipeline here.
+        .pipe(uglify())
+        .on('error', log.error)
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist/js/'))
+    )
+  })
 
-	restaurantInfoPageFiles.map(jsfile => {
-		return (
-			browserify({
-				entries: [jsfile]
-			})
-				.transform('babelify', { presets: ['env'] })
-				.bundle()
-				.pipe(source('restaurant.bundle.js')) // source works only for one file at a time
-				.pipe(buffer()) // convert streaming to buffered vinyl file objects
-				.pipe(sourcemaps.init({ loadMaps: true }))
-			// Add transformation tasks to the pipeline here.
-				.pipe(uglify())
-				.on('error', log.error)
-				.pipe(sourcemaps.write('./'))
-				.pipe(gulp.dest('dist/js'))
-		)
-	})
+  restaurantInfoPageFiles.map(jsfile => {
+    return (
+      browserify({
+        entries: [jsfile]
+      })
+        .transform('babelify', { presets: ['env'] })
+        .bundle()
+        .pipe(source('restaurant.bundle.js')) // source works only for one file at a time
+        .pipe(buffer()) // convert streaming to buffered vinyl file objects
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        // Add transformation tasks to the pipeline here.
+        .pipe(uglify())
+        .on('error', log.error)
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist/js'))
+    )
+  })
 })
 
 /*
    * Minify and autoprefix css
    */
-gulp.task('styles', () => {
-	gulp
-		.src('src/css/*.css')
-		.pipe(sourcemaps.init())
-		.pipe(
-			autoprefixer({
-				browsers: ['last 2 versions']
-			})
-		)
-		.pipe(
-			cleanCSS({
-				compatibility: 'ie8'
-			})
-		)
-		.pipe(
-			rename({
-				suffix: '.min'
-			})
-		)
-		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest('dist/css'))
+gulp.task('minify-css', () => {
+  gulp
+    .src('src/css/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions']
+      })
+    )
+    .pipe(
+      cleanCSS({
+        compatibility: 'ie8'
+      })
+    )
+    .pipe(
+      rename({
+        suffix: '.min'
+      })
+    )
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist/css'))
 })
 /*
    * Watch and rebuild js files
    */
-gulp.task('watch', () => {
-	gulp.watch(mainPageFiles, ['build'])
-	gulp.watch('./*.html').on('change', bs.reload)
+
+gulp.task('serve', () => {
+  bs.init({
+    server: './'
+  })
+  gulp.watch('src/js/*.js', ['minify-js'])
+  gulp.watch('src/js/*.js').on('change', reload)
+  gulp.watch('src/css/*.css', ['minify-css'])
+  gulp.watch('src/css/*.css').on('change', reload)
+  bs.stream()
 })
 
-gulp.task('browser-sync', () => {
-	bs.init({
-		server: './'
-	})
-	bs.stream()
-})
-
-gulp.task('default', ['watch'])
+gulp.task('default', ['minify-css', 'minify-js', 'serve'])
